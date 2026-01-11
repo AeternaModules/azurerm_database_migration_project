b10dae82446ec8f36cca04de85913bbb2a0a8a71
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:43:35 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "database_migration_projects" {
+  description = "All database_migration_project resources"
+  value       = azurerm_database_migration_project.database_migration_projects
+}
+
