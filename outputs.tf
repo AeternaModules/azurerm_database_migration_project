ba3e184c635a482ebf300a049792372c4ecf1d2f
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:08 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "database_migration_projects" {
-  description = "All database_migration_project resources"
-  value       = azurerm_database_migration_project.database_migration_projects
-}
 output "database_migration_projects_location" {
-  description = "List of location values across all database_migration_projects"
-  value       = [for k, v in azurerm_database_migration_project.database_migration_projects : v.location]
+  description = "Map of location values across all database_migration_projects, keyed the same as var.database_migration_projects"
+  value       = { for k, v in azurerm_database_migration_project.database_migration_projects : k => v.location }
 }
 output "database_migration_projects_name" {
-  description = "List of name values across all database_migration_projects"
-  value       = [for k, v in azurerm_database_migration_project.database_migration_projects : v.name]
+  description = "Map of name values across all database_migration_projects, keyed the same as var.database_migration_projects"
+  value       = { for k, v in azurerm_database_migration_project.database_migration_projects : k => v.name }
 }
 output "database_migration_projects_resource_group_name" {
-  description = "List of resource_group_name values across all database_migration_projects"
-  value       = [for k, v in azurerm_database_migration_project.database_migration_projects : v.resource_group_name]
+  description = "Map of resource_group_name values across all database_migration_projects, keyed the same as var.database_migration_projects"
+  value       = { for k, v in azurerm_database_migration_project.database_migration_projects : k => v.resource_group_name }
 }
 output "database_migration_projects_service_name" {
-  description = "List of service_name values across all database_migration_projects"
-  value       = [for k, v in azurerm_database_migration_project.database_migration_projects : v.service_name]
+  description = "Map of service_name values across all database_migration_projects, keyed the same as var.database_migration_projects"
+  value       = { for k, v in azurerm_database_migration_project.database_migration_projects : k => v.service_name }
 }
 output "database_migration_projects_source_platform" {
-  description = "List of source_platform values across all database_migration_projects"
-  value       = [for k, v in azurerm_database_migration_project.database_migration_projects : v.source_platform]
+  description = "Map of source_platform values across all database_migration_projects, keyed the same as var.database_migration_projects"
+  value       = { for k, v in azurerm_database_migration_project.database_migration_projects : k => v.source_platform }
 }
 output "database_migration_projects_tags" {
-  description = "List of tags values across all database_migration_projects"
-  value       = [for k, v in azurerm_database_migration_project.database_migration_projects : v.tags]
+  description = "Map of tags values across all database_migration_projects, keyed the same as var.database_migration_projects"
+  value       = { for k, v in azurerm_database_migration_project.database_migration_projects : k => v.tags }
 }
 output "database_migration_projects_target_platform" {
-  description = "List of target_platform values across all database_migration_projects"
-  value       = [for k, v in azurerm_database_migration_project.database_migration_projects : v.target_platform]
+  description = "Map of target_platform values across all database_migration_projects, keyed the same as var.database_migration_projects"
+  value       = { for k, v in azurerm_database_migration_project.database_migration_projects : k => v.target_platform }
 }
 
